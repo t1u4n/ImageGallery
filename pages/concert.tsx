50a8b8e246c5d0af4a5f29c7602c0eccd16169cc
Tian Luan <tiluan1999@hotmail.com>
2023-10-29 20:21:58 -0700

    feat: Downgrade nextjs version to 13 and add return link

--- pages/concert.tsx
+++ pages/concert.tsx
@@ -20,17 +20,22 @@ const Concert: NextPage = () => {
     <>
       <main className="mx-auto max-w-[1960px] p-4">
         <div className="columns-1 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4">
-          <div className="after:content relative mb-5 flex h-[629px] flex-col items-center justify-end gap-4 overflow-hidden rounded-lg bg-white/10 px-6 pb-16 pt-64 text-center text-white shadow-highlight after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight lg:pt-0">
-            <div className="absolute inset-0 flex items-center justify-center opacity-20">
-              <span className="flex max-h-full max-w-full items-center justify-center">
-                <Bridge />
-              </span>
-              <span className="absolute left-0 right-0 bottom-0 h-[400px] bg-gradient-to-b from-black/0 via-black to-black"></span>
+
+        <a href='/'>
+            <div className="after:content relative mb-5 flex h-[629px] flex-col items-center justify-end gap-4 overflow-hidden rounded-lg bg-white/10 px-6 pb-16 pt-64 text-center text-white shadow-highlight after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight lg:pt-0">
+                <div className="absolute inset-0 flex items-center justify-center opacity-20">
+                <span className="flex max-h-full max-w-full items-center justify-center">
+                    <Bridge />
+                </span>
+                <span className="absolute left-0 right-0 bottom-0 h-[400px] bg-gradient-to-b from-black/0 via-black to-black"></span>
+                </div>
+                <h1 className="mt-8 mb-4 text-base font-bold uppercase tracking-widest">
+                Concerts Gallery 🎶
+                </h1>
             </div>
-            <h1 className="mt-8 mb-4 text-base font-bold uppercase tracking-widest">
-              Concerts Gallery 🎶
-            </h1>
-          </div>
+        </a>
+    
+
           {images.map((image, index) => (
           <div key={index} className={styles.galleryItem}>
             <Image
